ecd82b2d45b45c9b25837c2b8f3fb902e42be5ff
Guoyinzh <guoyinzh@gmail.com>
2018-11-10 15:28:45 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+    
+setup(
+    name='geolearn',
+    version='0.1.1',
+    author='Guoyin Zhang',
+    author_email='guoyinzh@gmail.com',
+    packages=['geolearn',
+              'geolearn.test'],
+    description='Machine learning for geosciences',
+    license='LICENSE'
+    )
