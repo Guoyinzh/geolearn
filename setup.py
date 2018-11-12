564a103246c1fd326f8b2b7d8d8c88ab391e2450
Guoyinzh <guoyinzh@gmail.com>
2018-11-12 09:59:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
     
 setup(
     name='geolearn',
-    version='0.1.1',
+    version='0.0.1',
     author='Guoyin Zhang',
     author_email='guoyinzh@gmail.com',
     packages=['geolearn',
